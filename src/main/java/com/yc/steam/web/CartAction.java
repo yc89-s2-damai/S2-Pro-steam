aa9848c38fe946648ddb3bbae4fb448098dfc266
Lixiaolin0814 <57896766@qq.com>
2020-12-23 18:42:53 +0800

    船新版本1223

--- src/main/java/com/yc/steam/web/CartAction.java
+++ src/main/java/com/yc/steam/web/CartAction.java
@@ -1,5 +1,41 @@
 package com.yc.steam.web;
 
-public class CartAction {
+import java.util.List;
 
+import javax.annotation.Resource;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.yc.steam.dao.CartDao;
+import com.yc.steam.po.Result;
+import com.yc.steam.po.User;
+
+@RestController
+public class CartAction {
+	
+	
+	@Resource
+	private CartDao cdao;
+	/**
+	 * 查询购物车
+	 * @param session
+	 * @return
+	 */
+	@RequestMapping(path="cart.s" ,params = "op=queryCart")
+	public List<?> queryCart(HttpSession session){
+		User user = (User) session.getAttribute("loginedUser");
+		return cdao.selectCart(user.getUid());
+	}
+	/**
+	 * 取消订单
+	 * @param session
+	 * @return
+	 */
+	public Result ClearCart(HttpSession session) {
+		User user = (User) session.getAttribute("loginedUser");
+		cdao.deleteByUid(user.getUid());
+		return Result.success("取消成功！");
+	}
 }
